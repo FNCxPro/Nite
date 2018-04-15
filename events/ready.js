066ab0de9d3b430f3426075baef9f5d873c17124
Seth Stephens <relative@airmail.cc>
2018-04-15 17:50:39 -0400

    status

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,7 @@
 const logger = require('../logger')
+const version = require('../package.json').version
 module.exports = (client) => {
   logger.info(`${client.user.username} is ready.`)
   global.logchan = client.channels.get('435159402463625225')
+  client.user.setActivity(`Nite v${version} - !help`)
 }
