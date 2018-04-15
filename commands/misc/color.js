eebda3e83aa25b4e4038337832e06f0f90a4131d
Seth Stephens <relative@airmail.cc>
2018-04-15 17:02:07 -0400

    aaa

--- commands/misc/color.js
+++ commands/misc/color.js
@@ -30,7 +30,7 @@ module.exports = class ColorCommand extends Command {
     color = color.toLowerCase()
     let dbRs = await db.r.table('roles').get(color).run()
     let roleId
-    if (typeof dbRs !== 'undefined') {
+    if (!!dbRs) {
       roleId = dbRs.roleId
     } else {
       const role = await msg.guild.createRole({
